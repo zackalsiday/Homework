90346091cc0090bbe649fa8069e8bb905be0e5c8
zack alsiday <alsidayzack@gmail.com>
2021-11-16 07:41:30 -0800

    done

---
+++ w3d5/diy_adt.rb
@@ -0,0 +1,56 @@
+class Stack
+    def initialize
+      arr = []
+    end
+
+    def push(el)
+      arr.push(el)
+    end
+
+    def pop 
+      arr.pop 
+    end
+
+    def peek
+      arr.last 
+    end
+  end
+
+class Queue 
+  def initialize 
+    arr = []
+  end 
+
+  def enqueue(el)
+      arr.push(ele)
+  end 
+
+  def dequeue 
+    arr.shift 
+  end 
+
+  def peek 
+    arr[0]
+  end 
+
+end
+
+class Map 
+  def initialize
+    arr = []
+  end  
+
+  def set(key,value)
+  end
+
+  def get(key)
+  end 
+
+  def delete(key) 
+  end 
+
+  def show 
+  end
+
+
+end
